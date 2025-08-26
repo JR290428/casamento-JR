b47ec458da412854740adc7b2d2bcd3c80191040
JR290428 <rayanevelho1@gmail.com>
2025-08-26 17:09:25 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+const config = {
+  couple: "João & Rayane",
+  date: "2028-04-29T14:30:00",
+  rsvpDeadline: "2028-03-20",
+  dressCode: "Esporte fino",
+  pixKey: "CHAVE_PIX_AQUI",
+  gifts: [
+    { name: "Cota Lua de Mel", value: 200, link: "#" },
+    { name: "Jantar especial", value: 150, link: "#" }
+  ],
+  timeline: [
+    { time: "14:30", event: "Cerimônia" },
+    { time: "16:30", event: "Brunch" },
+    { time: "20:00", event: "After 🎶" }
+  ]
+};
